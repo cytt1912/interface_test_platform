5482b1962f01b1c94eb81a555c42c86047af8074
cytcyt <1649493622@qq.com>
2020-07-23 22:54:16 +0800

    接口自动化测试平台

---
+++ Interface_test_platform/Platform/views.py
@@ -0,0 +1,24 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+# import
+# Create your views here.
+
+def welcome(request):
+    print("欢迎进入首页！")
+    # return HttpResponse("欢迎进入首页！")
+    return render(request, "welcome.html")
+
+
+def caseList(request):
+    return render(request,"caseList.html")
+
+def home(request):
+    return render(request,'welcome.html',{"whichHTML": "Home.html"})
+
+# def home(request):
+#     return render(request,"Home.html")
+
+def child(request,eid):
+    return render(request,eid)
